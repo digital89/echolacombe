4b30d476ba45e7e256afca4257b5566a34bbfd05
Jared Reich <jaredreich@gmail.com>
2022-03-09 20:27:43 -0700

    Add more pages.

---
+++ src/pages/merch-your-way-program.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+import { PAGES } from '../constants';
+
+const C = () => {
+  return (
+    <>
+      <Layout>
+        <section className="section">
+          <div className="container content">
+            <h1>{PAGES.community.merchYourWayProgram.title}</h1>
+            <h2>Template subtitle...</h2>
+          </div>
+        </section>
+      </Layout>
+    </>
+  );
+};
+
+export default C;
