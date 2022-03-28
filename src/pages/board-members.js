25f1cb2865c53d84ec4d92e13722121cf91b81d3
Jared Reich <jaredreich@gmail.com>
2022-03-28 12:09:38 -0600

    Update board members.

--- src/pages/board-members.js
+++ src/pages/board-members.js
@@ -12,39 +12,48 @@ const C = () => {
             <h1>{PAGES.aboutUs.boardMembers.title}</h1>
             <ul>
               <li>
-                <span>President (Jared Reich – Member at Large representative)</span>
+                <span>Jared Reich (Jared Reich – Member at Large representative)</span>
                 <span> - </span>
                 <a href="mailto:jared@echolacombe.ca">jared@echolacombe.ca</a>
               </li>
               <li>
-                <span>Vice-President (Jana Fafard – Member at Large representative)</span>
+                <span>Jana Fafard (Vice-President – Member at Large representative)</span>
                 <span> - </span>
                 <a href="mailto:jana@echolacombe.ca">jana@echolacombe.ca</a>
               </li>
               <li>
-                <span>Director (Thalia Hibbs – City of Lacombe Council representative)</span>
+                <span>Thalia Hibbs (Director – City of Lacombe Council representative)</span>
                 <span> - </span>
                 <a href="mailto:thalia@echolacombe.ca">thalia@echolacombe.ca</a>
               </li>
               <li>
-                <span>Director (David Jeffrey – Burman University representative)</span>
+                <span>
+                  David Jeffrey (Director - David Jeffrey – Burman University representative)
+                </span>
                 <span> - </span>
                 <a href="mailto:david@echolacombe.ca">david@echolacombe.ca</a>
               </li>
               <li>
-                <span>Director (Thor Burnham – Member at Large representative)</span>
+                <span>Thor (Director Burnham – Member at Large representative)</span>
                 <span> - </span>
                 <a href="mailto:thor@echolacombe.ca">thor@echolacombe.ca</a>
               </li>
               <li>
-                <span>Director (Drayton Bussiere – Member at Large representative)</span>
-                <span> - </span>
-                <a href="mailto:drayton@echolacombe.ca">drayton@echolacombe.ca</a>
-              </li>
-              <li>
-                <span>Director (Kim May – Member at Large representative)</span>
+                <span>Kim Kay (Director – Member at Large representative)</span>
                 <span> - </span>
                 <a href="mailto:kim@echolaocmbe.ca">kim@echolaocmbe.ca</a>
+              </li>
+              <li>
+                <span>Barb Walker (Director – Member at Large representative)</span>
+                <span> - </span>
+                <a href="mailto:barb@echolacombe.ca">barb@echolacombe.ca</a>
+              </li>
+              <li>
+                <span>
+                  Guy Lapointe (Facilitator – City of Lacombe - Director of Community Services)
+                </span>
+                <span> - </span>
+                <a href="mailto:guy@echolacombe.ca">guy@echolacombe.ca</a>
               </li>
             </ul>
           </div>
