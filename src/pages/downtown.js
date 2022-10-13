247cf331920d1e89cf0aed561ad60a38f148977e
Jared Reich <jaredreich@gmail.com>
2022-10-12 21:14:08 -0600

    Add details to downtown page.

--- src/pages/downtown.js
+++ src/pages/downtown.js
@@ -10,9 +10,10 @@ const C = () => {
         <section className="section">
           <div className="container content">
             <h1>{PAGES.business.downtownBusinessEvents.title}</h1>
-            <p>
-              Page under construction...
-            </p>
+            <h3>1st Annual Downtown Spooktacular!</h3>
+            <p>October 27, 5:00 - 8:00 pm</p>
+            <p>Trick or Treating, Movies, Pumpkin Carving, Games, Colouring Contest, Hot Dogs, Family Fun and More!</p>
+            <p>More details and map coming soon!</p>
           </div>
         </section>
       </Layout>
