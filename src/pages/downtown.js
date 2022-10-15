02b04efc1fa4f1d6cb28ba14424c710f3355b4f8
Jared Reich <jaredreich@gmail.com>
2022-10-15 10:29:22 -0600

    Add image and pdf for spooktacular.

--- src/pages/downtown.js
+++ src/pages/downtown.js
@@ -10,10 +10,18 @@ const C = () => {
         <section className="section">
           <div className="container content">
             <h1>{PAGES.business.downtownBusinessEvents.title}</h1>
-            <h3>1st Annual Downtown Spooktacular!</h3>
-            <p>October 27, 5:00 - 8:00 pm</p>
+            <h3 className="title">🎃 1st Annual Downtown Spooktacular! 🎃</h3>
+            <p className="subtitle">October 27, 5:00 - 8:00 pm</p>
             <p>Trick or Treating, Movies, Pumpkin Carving, Games, Colouring Contest, Hot Dogs, Family Fun and More!</p>
-            <p>More details and map coming soon!</p>
+            <p>
+              <span>Map </span>
+              <a
+                href="/files/spooktacular-map.pdf"
+                target="_blank"
+              >(download)</a>
+              <span>:</span>
+              <img src="/images/spooktacular-map.jpg" alt="Spooktacular Map" />
+            </p>
           </div>
         </section>
       </Layout>
