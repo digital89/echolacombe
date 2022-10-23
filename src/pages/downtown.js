d338f1e5dd7bb486eaddaa1483be40c7c384a2f7
Jared Reich <jaredreich@gmail.com>
2022-10-23 07:24:38 -0600

    Updates to downtown.js

--- src/pages/downtown.js
+++ src/pages/downtown.js
@@ -35,28 +35,27 @@ const C = () => {
                 <li><b>Dutchess Flower Shop:</b> 20% off all plants and wall art</li>
                 <li><b>Milly Oak Cafe and Catering:</b> Pumpkin Decorating</li>
                 <li><b>Broom Tree Foundation:</b> Ghost Painting Activity + Carmel Apples for purchase</li>
-                <li><b>Elite Bridal:</b> Trick or Treating Treats</li>
-                <li><b>The Grooming Shop:</b> Trick or Treating Treats (**open until 7PM)</li>
+                <li><b>Elite Bridal:</b> Trick-or-treating Treats</li>
+                <li><b>The Grooming Shop:</b> Trick-or-treating Treats (**open until 7PM)</li>
                 <li><b>Breathe Massage and Wellness:</b> Treats and Apple Cider (**open until 7PM)</li>
                 <li><b>Downton & Co.:</b> 10% off candles & Fragrances  as well as a scavenger hunt for the kids. ( We will have mini pumpkins hidden around the store and kids that find them will get a free winnies ice cream, goodie bags will be available for any kids with allergies)</li>
-                <li><b>ONA’s:</b> Free Chai Lattes & trick or treating</li>
+                <li><b>ONA’s:</b> Free Chai Lattes & Trick-or-treating</li>
                 <li><b>Winnie’s Ice Cream:</b> 15% off Ice Cream</li>
                 <li><b>Ugly’s Pub:</b> Trick-or-treating and Wing Night</li>
-                <li><b>GiftSmack:</b> Trick Or Treating</li>
-                <li><b>Now Home Hardware:</b> Trick-or-Treating and “Pick the Lock” game</li>
-                <li><b>SUNNY 94:</b> Advertising</li>
+                <li><b>GiftSmack:</b> Trick-or-treating</li>
+                <li><b>Nowco Home Hardware:</b> Trick-or-treating and “Pick the Lock” game</li>
                 <li><b>Fire Department:</b> BBQ</li>
                 <li><b>Lacombe Museum:</b> Michener House (Flat Iron closed) shortened version of a Downtown Tour at 5:30 and 7:30. Our vegetable garden will also be set up as a selfie station/photo booth.</li>
                 <li><b>Hometown Market:</b> Halloween coloring contest, will announce on our social the week leading up, coloring sheet options available in store all that day/night.  We'll also have a coloring station set up for kids to color while the adults shop.</li>
-                <li><b>Rae’ven Co. and Wolf & Raven:</b> Trick or Treating and Balloons</li>
+                <li><b>Rae’ven Co. and Wolf & Raven:</b> Trick-or-treating and Balloons</li>
                 <li><b>HeadHunters:</b> Trick-or-treating and offer 15% off in store.</li>
-                <li><b>Red’s Source for Sports:</b> Offering an extra 5% discount on purchases made between 6pm and 8pm for customers dressed up in a costume.</li>
-                <li><b>Big Brothers Big Sisters:</b> Trick-or-Treating</li>
+                <li><b>Red’s Source for Sports:</b>Trick-or-treating and offering an extra 5% discount on purchases made between 6pm and 8pm for customers dressed up in a costume.</li>
+                <li><b>Big Brothers Big Sisters:</b> Trick-or-treating</li>
                 <li><b>Jo(e) Social Media:</b> Movie Scene Filming</li>
                 <li><b>Tollers Bistro:</b> Costume Contest. Winners of group costumes receive a dozen donuts, solo costume will receive ½ dozen donuts.</li>
                 <li><b>Mackenzie @ Cin City:</b> Face painting</li>
                 <li><b>Leto’s:</b> “Happiest Hours” to 9pm - $2 off appetizers/ $5 highballs / $1 off 6oz. Red/White Wine / Pints $7.5</li>
-                <li><b>Collective Mayhem:</b> Trick or Treating</li>
+                <li><b>Collective Mayhem:</b> Trick-or-treating</li>
               </ul>
             </p>
           </div>
