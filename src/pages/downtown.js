3d8f85e3f352efd06041596546459c7289d7a3d6
Jared Reich <jaredreich@gmail.com>
2022-11-18 10:24:12 -0700

    Updates for moonlight madness.

--- src/pages/downtown.js
+++ src/pages/downtown.js
@@ -10,52 +10,66 @@ const C = () => {
         <section className="section">
           <div className="container content">
             <h1>{PAGES.business.downtownBusinessEvents.title}</h1>
-            <h3 className="title">🎃 1st Annual Downtown Spooktacular! 🎃</h3>
-            <p className="subtitle">October 27, 5:00 - 8:00 pm</p>
-            <p>Trick or Treating, Movies, Pumpkin Carving, Games, Colouring Contest, Hot Dogs, Family Fun and More!</p>
+            <h3 className="title">🌛 Moonlight Madness 🌛</h3>
+            <p className="subtitle">November 24th, 2022</p>
+            {/* <p>More details...</p> */}
             <p>
               <h4>
                 <span>Map </span>
                 <a
-                  href="/files/spooktacular-map.pdf"
+                  href="/files/moonlight-madness-map.pdf"
                   target="_blank"
                 >(download / view full)</a>
                 <span>:</span>
               </h4>
-              <img src="/images/spooktacular-map.jpg" alt="Spooktacular Map" style={{ maxWidth: '600px', width: '100%' }}/>
+              <img src="/images/moonlight-madness-map.jpg" alt="Moonlight Madness Map" style={{ maxWidth: '600px', width: '100%' }}/>
             </p>
             <h4>Details:</h4>
             <p>
               <ul>
                 <li><b>5024 General Store:</b> Candy, popcorn and Classic Halloween Specials showing on the projector.</li>
-                <li><b>Oodles of Chocolates:</b> Free hot chocolate, treats and Take Home Oreo Kits for the first 75 kids.</li>
-                <li><b>Blindman Brewery:</b> Photo Booth and S’mores around the fire.</li>
-                <li><b>Wags Holistic Pet & Passionate Paws:</b> Pet photos for a $10 donation for charity</li>
-                <li><b>Anna Maria’s Cafe:</b> Photo Opportunity and Treats (open in LMC during farmers market)</li>
-                <li><b>Dutchess Flower Shop:</b> 20% off all plants and wall art</li>
-                <li><b>Milly Oak Cafe and Catering:</b> Pumpkin Decorating</li>
-                <li><b>Broom Tree Foundation:</b> Ghost Painting Activity + Carmel Apples for purchase</li>
-                <li><b>Elite Bridal:</b> Trick-or-treating Treats</li>
-                <li><b>The Grooming Shop:</b> Trick-or-treating Treats (**open until 7PM)</li>
-                <li><b>Breathe Massage and Wellness:</b> Treats and Apple Cider (**open until 7PM)</li>
-                <li><b>Downton & Co.:</b> 10% off candles & Fragrances  as well as a scavenger hunt for the kids. ( We will have mini pumpkins hidden around the store and kids that find them will get a free winnies ice cream, goodie bags will be available for any kids with allergies)</li>
-                <li><b>ONA’s:</b> Free Chai Lattes & Trick-or-treating</li>
-                <li><b>Winnie’s Ice Cream:</b> 15% off Ice Cream</li>
-                <li><b>Ugly’s Pub:</b> Trick-or-treating and Wing Night</li>
-                <li><b>GiftSmack:</b> Trick-or-treating</li>
-                <li><b>Nowco Home Hardware:</b> Trick-or-treating and “Pick the Lock” game</li>
-                <li><b>Fire Department:</b> BBQ</li>
-                <li><b>Lacombe Museum:</b> Michener House (Flat Iron closed) shortened version of a Downtown Tour at 5:30 and 7:30. Our vegetable garden will also be set up as a selfie station/photo booth.</li>
-                <li><b>Hometown Market:</b> Halloween coloring contest, will announce on our social the week leading up, coloring sheet options available in store all that day/night.  We'll also have a coloring station set up for kids to color while the adults shop.</li>
-                <li><b>Rae’ven Co. and Wolf & Raven:</b> Trick-or-treating and Balloons</li>
-                <li><b>HeadHunters:</b> Trick-or-treating and offer 15% off in store.</li>
-                <li><b>Red’s Source for Sports:</b>Trick-or-treating and offering an extra 5% discount on purchases made between 6pm and 8pm for customers dressed up in a costume.</li>
-                <li><b>Big Brothers Big Sisters:</b> Trick-or-treating</li>
-                <li><b>Jo(e) Social Media:</b> Movie Scene Filming</li>
-                <li><b>Tollers Bistro:</b> Costume Contest. Winners of group costumes receive a dozen donuts, solo costume will receive ½ dozen donuts.</li>
-                <li><b>Mackenzie @ Cin City:</b> Face painting</li>
-                <li><b>Leto’s:</b> “Happiest Hours” to 9pm - $2 off appetizers/ $5 highballs / $1 off 6oz. Red/White Wine / Pints $7.5</li>
-                <li><b>Collective Mayhem:</b> Trick-or-treating</li>
+
+                <li><b>Elite Bridal:</b> We will be open late (usually until 10 pm). We will have moonlight madness sales and Black Friday sales that night.</li>
+
+                <li><b>Downton & Co. and ONA Café:</b> will be open and celebrating Moonlight Madness.</li>
+
+                <li><b>Letos:</b> We will be open, offering buy one get one 1/2 off appetizers from 5-9 pm with the purchase of a beverage. We will match the discounted amount and donate to FCSS Lacombe - Santa's anonymous. In addition, we will be offering a GIFT CARD sale. For every $100 GC purchase, an additional $20 GC will
+                be included.</li>
+
+                <li><b>Oodles of Chocolates:</b> will be open to the public. Our shop will be filled with all your favourite Christmas Treats. We will also be offering our delicious Hot Chocolate for purchase.</li>
+
+                <li><b>5024 General Store:</b> will be participating with a shopping discount of 10%, and every purchase will be an entry to win one of our Christmas Boxes that includes a bag of 5024 coffee, a gift card, a mug, and other goodies.</li>
+
+                <li><b>The Grooming Shop:</b> will be open and celebrating Moonlight Madness.</li>
+
+                <li><b>Anna Maria's Café:</b> will be open and celebrating Moonlight Madness.</li>
+
+                <li><b>Headhunters:</b> will be open and extending the sale for that week.</li>
+
+                <li><b>Nowco Home Hardware:</b> will be open and celebrating Moonlight Madness.</li>
+
+                <li><b>The Broom Tree Café:</b> will be open and celebrating Moonlight Madness.</li>
+
+                <li><b>Health fitters:</b> will be open and celebrating Moonlight Madness.</li>
+
+                <li><b>Reds Source for Sports:</b> will be open and celebrating Moonlight Madness.</li>
+
+                <li><b>The Studio by Elysian & Luxe:</b> will be open and celebrating Moonlight Madness. We are offering retail sales, a giveaway basket or two, and a gift with purchase.</li>
+
+                <li><b>Chamber of Commerce:</b> It's double stamp day, with the Passport to Christmas too!</li>
+
+                <li><b>Uglys:</b> will be doing our wing night and drink specials from 4-11 pm; we will also do 10% off of all of our merchandise... hoodies, crewneck sweaters, short sleeves, long sleeves, tank tops, toques, ball caps, and lanyards.</li>
+
+                <li><b>Copper Lane:</b> will be hosting a curated pop-up from 6-9 pm (but will stay open later if needed) with
+                wine tasting by LA liquor and snacks by honey b charcuterie. 20% off all items in store and we hope to have propane fire pits out front for smores.</li>
+
+                <li><b>Dutchess Flower Company:</b> will be open, celebrating Moonlight Madness and we will have a 20% off
+                sale on houseplants and wall art.</li>
+
+                <li><b>Wags Holistic Pet Nutrition:</b> will be open until 8 pm and celebrating Moonlight Madness.</li>
+
+                <li><b>RAE'VeN Co. Apothecary:</b> will stay open until 8:30 and we will be doing a craft of making Yuletide
+                intention pinecones from 6-8.</li>
               </ul>
             </p>
           </div>
